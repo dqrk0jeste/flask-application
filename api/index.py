d6fde94126a2c33f751be84556b3afbcba83dad0
darko <darkonikoloc@gmail.com>
2023-12-08 21:19:07 +0100

    fixed database connection

--- api/index.py
+++ api/index.py
@@ -3,52 +3,78 @@ from flask import Flask
 import mysql.connector
 from dotenv import load_dotenv
 
+#load enviroment variables
 load_dotenv()
 
+#create app
 app = Flask(__name__)
-
-db_connection = mysql.connector.connect(
-    database =  os.environ.get('DB_NAME'),
-    host = os.environ.get('DB_HOST'),
-    port = os.environ.get('DB_PORT'),
-    user = os.environ.get('DB_USER'),
-    password = os.environ.get('DB_PASSWORD')
-)
-
-print("connected to a database")
-
-cursor = db_connection.cursor(dictionary = True)
-
-cursor.execute("""
-    DROP TABLE Test;
-""")
-
-cursor.execute("""
-    CREATE TABLE Test(
-        ime VARCHAR(255) NOT NULL
-    );
-""")
-
-cursor.execute("""
-    INSERT INTO Test(ime)
-    VALUES ('darko');
-""")
-
-
-
 
 @app.route('/')
 def home():
+    #connect to a database, should do this in every serverless function
+    #remember to close the connection before returning
+    connection = connectDatabase()
+    if(connection):
+        cursor = createCursor(connection)
+    else:
+        return "there has been an error connecting to a database"
+
+    #drop existing table
+    cursor.execute("""
+        DROP TABLE Test;
+    """)
+
+    #create mock table
+    cursor.execute("""
+        CREATE TABLE Test(
+            ime VARCHAR(255) NOT NULL
+        );
+    """)
+
+    #insert mock value
+    cursor.execute("""
+        INSERT INTO Test(ime)
+        VALUES ('darko');
+    """)
+
+    #retrieve data from a database
     cursor.execute("""
         SELECT * FROM Test;
     """)
 
     results = cursor.fetchall()
 
-    print(results)
+    closeCursor(cursor)
+    dissconnectDatabase(connection)
 
     return f"<h1>Zdravo, {results[0]['ime']}!</h1>"
 
 @app.route('/about')
 def about():
     return 'About'
+
+#utils, I don't know how to make Vercel not pick them up as serverless functions, should probably be in the documentation.
+def connectDatabase():
+    try:
+        connection = mysql.connector.connect(
+            database =  os.environ.get('DB_NAME'),
+            host = os.environ.get('DB_HOST'),
+            port = os.environ.get('DB_PORT'),
+            user = os.environ.get('DB_USER'),
+            password = os.environ.get('DB_PASSWORD')
+            #ssl_ca = os.environ.get('MYSQL_ATTR_SSL_CA') #uncomment in production to enable secured connection
+        )
+        print("connected to a database")
+        return connection
+    except:
+        print("there has been an error")
+        return None
+
+def dissconnectDatabase(connection):
+    connection.close()
+
+def createCursor(connection):
+    return connection.cursor(dictionary = True)
+
+def closeCursor(cursor):
+    cursor.close()
